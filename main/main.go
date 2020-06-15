c51f883d78bf369da1fc609c0e61d613304bc5f6
mutun <tun224813@gmail.com>
2020-06-15 16:35:57 +0800

    使用原生的http包返回json数据

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	".."
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,12 +14,25 @@ func main() {
 
 	// 自定义Engine处理所有请求 engine实现的serveHTTP方法 等同于实现了handler的接口
 	e := tequila.New()
-	e.Post("/testPost", func(w http.ResponseWriter, r *http.Request) {
+	e.Post("/post", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "this is  post method %s", r.URL)
 	})
 
-	e.Get("/testGet", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "this is get method %s", r.URL.Path)
+	// 返回一个json的原生写法
+	e.Get("/get", func(w http.ResponseWriter, r *http.Request) {
+		// 生成json对象
+		obj := map[string]interface{}{
+			"name": "joey",
+			"age":  30,
+		}
+		// 设置返回格式 返回状态
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(obj); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	e.Get("/", func(w http.ResponseWriter, r *http.Request) {
